30d82f2e1de811badc2df124ea99843a51e6ce0a
maxdoubt <benjamin.racher@gmail.com>
2015-10-30 15:51:22 -0500

    Config Tweaks

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,11 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w(rails_admin/custom/theming.css rails_admin/custom/ui.js)
